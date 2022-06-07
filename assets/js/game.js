937bfce81d7a52d9e7c7da5a80180d8f31cc7741
backspace64 <cheezydixon@gmail.com>
2022-06-07 18:38:13 -0500

    robot name

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+// this creates a funtion named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName=window.prompt("What is your robot's name");
+// What is this?
+console.log(playerName);
+
+
+
+
+//fight();
